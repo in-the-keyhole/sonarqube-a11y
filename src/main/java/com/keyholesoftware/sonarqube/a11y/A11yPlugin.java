fb9f603495a596a63dcc98f2fada27a67b603d1a
Adi Rosenblum <arosenblum@keyholesoftware.com>
2025-01-20 14:32:50 -0600

    setting up initial plugin structure

---
+++ src/main/java/com/keyholesoftware/sonarqube/a11y/A11yPlugin.java
@@ -0,0 +1,13 @@
+package com.keyholesoftware.sonarqube.a11y;
+
+import org.sonar.api.Plugin;
+
+public class A11yPlugin implements Plugin{
+
+    @Override
+    public void define(Context arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'define'");
+    }
+
+}
